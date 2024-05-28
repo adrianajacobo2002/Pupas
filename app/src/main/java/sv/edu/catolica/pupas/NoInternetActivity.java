56175609862bb30fd469fa1ee0626e07dc6ecb1b
Adriana Jacobo <adriana.jacobo@catolica.edu.sv>
2024-05-28 10:28:15 -0600

    Se agregó layout para cuando no haya conexión a internet

---
+++ app/src/main/java/sv/edu/catolica/pupas/NoInternetActivity.java
@@ -0,0 +1,14 @@
+package sv.edu.catolica.pupas;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class NoInternetActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_no_internet);
+    }
+}
