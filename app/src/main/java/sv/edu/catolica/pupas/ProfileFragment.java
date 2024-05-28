3836e5528cdb02a876a53b8568b0f4406d21075f
Diego Saravia <diego.saraviazaldana@gmail.com>
2024-05-27 23:35:43 -0600

    feat: cargando email y nombre con el usuario logeado actualmente

--- app/src/main/java/sv/edu/catolica/pupas/ProfileFragment.java
+++ app/src/main/java/sv/edu/catolica/pupas/ProfileFragment.java
@@ -7,14 +7,33 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+
+import helpers.PersistentData;
+import models.User;
 
 
 public class ProfileFragment extends Fragment {
 
+    private TextView tvFullName, tvEmail;
+
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        View view = inflater.inflate(R.layout.fragment_profile, container, false);
+
+        this.tvFullName = view.findViewById(R.id.tvFullName);
+        this.tvEmail = view.findViewById(R.id.tvEmail);
+
+        PersistentData persistentData = new PersistentData(getActivity());
+        try {
+            User user = persistentData.getObject(getString(R.string.user_sp_key), User.class);
+            this.tvFullName.setText(String.format("%s %s", user.names, user.lastNames));
+            this.tvEmail.setText(user.email);
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+
+        return view;
     }
 }
